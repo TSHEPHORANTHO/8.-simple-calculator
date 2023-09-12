6b2b37b09cfa8c162b2f1b1fa6cd691293444d76
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-12 10:34:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = num1 + num2;
+
+            Console.WriteLine("Results");
+            Console.WriteLine(num1 + " + " + num2 + " = " + num3);
+            //print
+            Console.ReadLine();
+
+
+        }
+    }
+}
